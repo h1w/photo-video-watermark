377e6ab85b3c5b7bbf8d68a3519c35d6e2d9a9f6
bpqq <bpqvgq@gmail.com>
2021-06-16 21:43:54 +0300

    Update settings: help, watermark

--- settings.py
+++ settings.py
@@ -1,3 +1,26 @@
 bot = {
     'token': open('TOKEN', 'r').readline() # Bot token
 }
+
+watermark = {
+    'watermark_default_text': "@boxthread",
+    'watermark_default_rgba': (150, 150, 150, 220),
+    'watermark_default_rgba_presets': """(150, 150, 150, 220)
+(0, 0, 0, 255)
+(30, 250, 90, 220)
+"""
+}
+
+help = {
+    'help_info': """photo
+video
+link photo{jpeg,png},video{mp4,webm}.
+""",
+    'photo_help_answer': """
+RGBA input format:
+R, G, B, A - Example: 24, 210, 8, 217
+(You can send rgba with () and without spaces.)
+Presets:
+{}
+""".format(watermark['watermark_default_rgba_presets'])
+}
